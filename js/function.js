4baeb4c2f398f243aeb760accb31edc97ad11c89
FreelancerShijan <freelancershijan@gmail.com>
2022-09-03 14:44:58 +0600

    all categories fetch done

--- js/function.js
+++ js/function.js
@@ -36,61 +36,67 @@ const loadNews = async category_id => {
 
 
 
-const displayNews = categories => {
+const displayNews = allnews => {
+
+
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = '';
+
+    allnews.forEach(news => {
+        console.log(news);
+
+        let i = 0; i < news.length; i++;
+        // console.log(selectedArray[i].playerName);
+        const name = news[i];
+        console.log(name);
 
 
 
-    categories.forEach(category => {
-        console.log(category);
-
-
-
-
-        const newsContainer = document.getElementById('news-container');
-        newsContainer.innerHTML = '';
+        // const newsContainer = document.getElementById('news-container');
+        /* newsContainer.innerHTML = ''; */
         const createSingleNews = document.createElement('div');
         createSingleNews.classList.add('single-news');
         createSingleNews.innerHTML = `
-      <div class="row g-0 m-3 rounded">
-         <div class="col-md-3">
-            <img src="${category.image_url}" class="img-fluid rounded" alt="...">
-         </div>
-         <div class="col-md-9 ">
-             <div class="card-body">
-                <h3 class="card-title fw-bold">${category ? category.title : 'No Data Found'}</h3>
-                <p class="card-text">${category.details.slice(0, 1000)}</p>
- 
-              </div>
-          <div class="card-bottom d-flex align-items-center justify-content-between">
-                  <div class="d-flex ">
-                          <img src="${category.author.img}" alt="">
-                          <div class="author">
-                              <h6>${category.author ? category.author.name : 'No Data Found'}</h6>
-                              <p>${category.author ? category.author.published_date : 'No Data Found'} </p>
-                          </div>
-                  </div>
- 
-                  <div>
-                          <h5><i class="fa-regular fa-eye"></i> ${category.total_view}</h5>
-                  </div>
-                  <div>
-                           <i class="fa-solid fa-star-half-stroke"></i>
-                           <i class="fa-regular fa-star"></i>
-                           <i class="fa-regular fa-star"></i>
-                           <i class="fa-regular fa-star"></i>
-                           <i class="fa-regular fa-star"></i>
-                           
-                  </div>
- 
-                 <div>
-                  <button onclick="loadDetails('${category._id}')" data-bs-toggle="modal" data-bs-target="#newsDetailModal"><i class="fa-solid fa-arrow-right"></i></button>
-                  
+              <div class="row g-0 m-3 rounded">
+                 <div class="col-md-3">
+                    <img src="${news.image_url}" class="img-fluid rounded" alt="...">
                  </div>
- 
-          </div>
-      </div>
- 
-      `;
+                 <div class="col-md-9 ">
+                     <div class="card-body">
+                        <h3 class="card-title fw-bold">${news ? news.title : 'No Data Found'}</h3>
+                        <p class="card-text">${news.details.slice(0, 1000)}</p>
+         
+                      </div>
+                  <div class="card-bottom d-flex align-items-center justify-content-between">
+                          <div class="d-flex ">
+                                  <img src="${news.author.img}" alt="">
+                                  <div class="author">
+                                      <h6>${news.author ? news.author.name : 'No Data Found'}</h6>
+                                      <p>${news.author ? news.author.published_date : 'No Data Found'} </p>
+                                  </div>
+                          </div>
+         
+                          <div>
+                                  <h5><i class="fa-regular fa-eye"></i> ${news.total_view}</h5>
+                          </div>
+                          <div>
+                                   <i class="fa-solid fa-star-half-stroke"></i>
+                                   <i class="fa-regular fa-star"></i>
+                                   <i class="fa-regular fa-star"></i>
+                                   <i class="fa-regular fa-star"></i>
+                                   <i class="fa-regular fa-star"></i>
+                                   
+                          </div>
+         
+                         <div>
+                          <button onclick="loadDetails('${news._id}')" data-bs-toggle="modal" data-bs-target="#newsDetailModal"><i class="fa-solid fa-arrow-right"></i></button>
+                          
+                         </div>
+         
+                  </div>
+              </div>
+         
+              `;
         newsContainer.appendChild(createSingleNews);
 
     })
@@ -118,14 +124,14 @@ const displayNewsDetails = categories => {
         newsTitle.innerText = category.title;
         const newsDetails = document.getElementById('news-details');
         newsDetails.innerHTML = `
-        <img src=${category.image_url} class="mb-3"/>
+        <img src=${category.image_url} class="mb-3 w-100"/>
         <h3>${category.title}</h3>
         <p>${category.details}</p>
        
 
         <div class="card-bottom d-flex align-items-center justify-content-between">
                   <div class="d-flex ">
-                          <img class=w-100"" src="${category.author.img}" alt="">
+                          <img  src="${category.author.img}" alt="">
                           <div class="author">
                               <h6>${category.author ? category.author.name : 'No Data Found'}</h6>
                               <p>${category.author ? category.author.published_date : 'No Data Fpund'} </p>
@@ -172,7 +178,14 @@ const toggleSpinner = isLoading => {
 
 
 
+const newsArray = [];
+
+
 allCategory();
+
+const showNewsLength = () => {
+
+}
 
 // loadCategory('01');
 
